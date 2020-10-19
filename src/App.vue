88ba2f30e413fc4ad1dd71acc96c26bf1648da59
Ghada <ghada.j@najah.edu>
2020-10-19 14:43:12 +0300

    "Components"

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Latest Posts from  our Blog</h1>
+    <Post title="Here I display latest blog's posts"></Post>
+    <h1>All Polls</h1>
+    <Polls title="Here I display All Polls"></Polls>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Post from "@/components/Post";
+import Polls from "@/components/Polls";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Post,
+    Polls
   }
 }
 </script>
---
+++ src/components/Polls.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <ul>
+      <li>
+        <div>Lorem ipsum dolor sit amet, consectetur.</div>
+        <div>At culpa deleniti deserunt facilis sit.</div>
+        <div>Adipisci aperiam consequatur non vitae voluptatibus.</div>
+        <div>Animi expedita natus obcaecati perferendis sed.</div>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Polls',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
